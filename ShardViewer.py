3a56983f4fbd9f07a731985c84bc00e713d26921
DotRar <dotrar@protonmail.com>
2018-04-28 18:07:36 +0100

    Initial commit

---
+++ ShardViewer.py
@@ -0,0 +1,33 @@
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from quart import Quart
+
+import urllib.request
+import json
+
+app = Quart(__name__)
+
+env = Environment(
+    loader = FileSystemLoader('templates'),
+    autoescape = select_autoescape(['html'])
+)
+
+totalShards = 20
+
+@app.route('/')
+async def index():
+    with urllib.request.urlopen('http://localhost:3333/shard/all') as response:
+        raw = json.loads(response.read())
+        parsed = {}
+
+        #Insert broken shards at the beginning
+        for key in filter(lambda x: raw[str(x)] != "CONNECTED", sorted(map(lambda x: int(x), raw.keys()))):
+            parsed[str(key)] = raw[str(key)]
+
+        for key in sorted(map(lambda x: int(x), raw.keys())):
+            if key not in parsed.keys():
+                parsed[str(key)] = raw[str(key)]
+
+        return env.get_template('index.html').render(parsed=parsed)
+
+
+app.run(host="0.0.0.0", port=5000)
---
+++ static/index.js
@@ -0,0 +1,72 @@
+var totalShards = 336;
+
+var ws = new WebSocket("ws://localhost:3333/ws");
+
+ws.onmessage = function(evt) {
+    var msg = evt.data.toString();
+    var parsed = JSON.parse(msg);
+    var id = parsed["ID"],
+        status = parsed["Status"];
+
+    var table = document.getElementById("statuses"),
+        tbody = table.tBodies[0];
+    $('tr').each(function(i, el) {
+        if(el.parentElement.tagName !== "THEAD") {
+            var $tds = $(this).find('td'),
+                elementId = $tds.eq(0).text(),
+                currentStatus = $tds.eq(1).text();
+
+            if(elementId == id) {
+                if (currentStatus !== status) {
+                    el.remove();
+                    if (status !== "CONNECTED") {
+                        var tr = tbody.insertRow(0);
+
+                        var idTd = document.createElement('td');
+                        idTd.innerHTML = id;
+
+                        var statusTd = document.createElement('td');
+                        statusTd.innerHTML = status;
+
+                        tr.appendChild(idTd);
+                        tr.appendChild(statusTd);
+                        $(tr).children('td').addClass("error");
+                        $(tr).children('td').addClass("shard-" + id);
+                    }
+                    else {
+                        var tr = tbody.insertRow(id);
+
+                        var idTd = document.createElement('td');
+                        idTd.innerHTML = id;
+
+                        var statusTd = document.createElement('td');
+                        statusTd.innerHTML = status;
+
+                        tr.appendChild(idTd);
+                        tr.appendChild(statusTd);
+                        $(tr).children('td').addClass("positive");
+                        $(tr).children('td').addClass("shard-" + id);
+                    }
+                }
+            }
+
+        }
+    });
+};
+
+function colourShards() {
+    $('tr').each(function(i, el) {
+        var $tds = $(this).find('td'),
+            id = $tds.eq(0).text(),
+            status = $tds.eq(1).text();
+
+        if(status !== "CONNECTED") {
+            $tds.addClass("error")
+        }
+        else {
+            $tds.addClass("positive");
+        }
+    });
+}
+
+colourShards();
